07664ae53004eecdc7e5354818d467bd58710f15
Bohdan Koltok <bogdan.koltok@gmail.com>
2022-11-21 00:18:42 +0200

    :construction: fix bug while booking creation :construction:

--- src/main/java/com/intellias/intellistart/interviewplanning/controllers/CoordinatorController.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/controllers/CoordinatorController.java
@@ -187,8 +187,24 @@ public class CoordinatorController {
         .build();
   }
 
+// Using model Mapper we get exception:
+//The destination property com.intellias.intellistart.interviewplanning.models.Booking.setId() matches multiple source property hierarchies:
+//	com.intellias.intellistart.interviewplanning.dto.BookingRequestDto.getInterviewerTimeSlotId()
+//	com.intellias.intellistart.interviewplanning.dto.BookingRequestDto.getCandidateTimeSlotId()
+//
+//  public Booking mapToBooking(BookingRequestDto bookingDto) {
+//    return modelMapper.map(bookingDto, Booking.class);
+//  }
+
   public Booking mapToBooking(BookingRequestDto bookingDto) {
-    return modelMapper.map(bookingDto, Booking.class);
+    return Booking.builder()
+        .candidateTimeSlotId(bookingDto.getCandidateTimeSlotId())
+        .interviewerTimeSlotId(bookingDto.getInterviewerTimeSlotId())
+        .from(bookingDto.getFrom())
+        .to(bookingDto.getTo())
+        .description(bookingDto.getDescription())
+        .subject(bookingDto.getSubject())
+        .build();
   }
 
 }
